e67a5a518e1b4b4dbb60d627a7fb05540852ad27
Aran <aran@ermarian.net>
2013-03-15 10:30:05 -0700

    Add Ajax to Markov Word script. Fix code style and HTML in letters script.

--- markov/letters.php
+++ markov/letters.php
@@ -10,10 +10,10 @@ $_REQUEST += [
   'json' => FALSE,
 ];
 
-$input = substr($_REQUEST['input'], 0, 100000);
-$length = min($_REQUEST['length'], 2000);
+$input     = substr($_REQUEST['input'], 0, 100000);
+$length    = min($_REQUEST['length'], 2000);
 $coherence = min($_REQUEST['coherence'], 20);
-$output = '';
+$output    = '';
 
 if ($input) {
   $relations = l_build_relations($input, $coherence);
@@ -30,23 +30,24 @@ if ($_REQUEST['json']) {
 }
 
 $page['title'] = 'Markov Letters';
-$page['scripts'] = ['style/jquery', 'style/jquery.form', 'style/ajax'];
 $page['meta']['description'] = 'Generate a random string from a source text, using a Markov chain algorithm.';
 $page['meta']['keywords'] = ['markov chain', 'random', 'generator', 'convert', 'arancaytar', 'ermarian'];
-$page['content'] = 
-<<<DOC
+
+$page['scripts'] = ['style/jquery', 'style/jquery.form', 'style/ajax'];
+
+$page['content'] = <<<DOC
     <p>This page will process your input texts and, using statistics and random numbers, will generate a string of output with letters that are approximately pronouncable.</p>
+
     <form action='' method='post'>
-    <input type='hidden' name='json' value='' />
-    <p><textarea name="input" rows="5" style="width:100%">$input</textarea></p>
-    <p>Generate <input type="text" name="length" value="$length" /> characters.</p>
-    <p>Chain length: <input type="text" name="coherence" value="$coherence" /> characters. (Lower = less coherent, higher = less deviation from the input text. Anything above 10 is likely to result in a word-for-word excerpt, depending on input size.)</p>
-    <p><input type="submit" value="Generate" /></p>
+      <input type='hidden' name='json' value='' />
+      <p><textarea name="input" rows="5" style="width:100%">$input</textarea></p>
+      <p>Generate <input type="text" name="length" value="$length" /> characters.</p>
+      <p>Chain length: <input type="text" name="coherence" value="$coherence" /> characters. (Lower = less coherent, higher = less deviation from the input text. Anything above 10 is likely to result in a word-for-word excerpt, depending on input size.)</p>
+      <p><input type="submit" value="Generate" /></p>
+    </form>
 
     <h3>Output</h3>
-    <p>
-      <div id="output" class="filled-box codeblock" style="white-space:pre-line">$output</div
-    </form>
+    <div id="output" class="filled-box codeblock" style="white-space:pre-line">$output</div>
 DOC;
 
 print theme_page($page);
--- markov/words.php
+++ markov/words.php
@@ -1,47 +1,52 @@
 <?php
 require_once 'markov.inc';
+require_once 'net/mime.inc';
 require_once 'ermarian/template.php';
-require_once 'template/navigation.inc';
 
-$_POST += array(
+$_REQUEST += [
   'input' => '',
   'length' => 100,
   'coherence' => 2,
-);
+  'json' => FALSE,
+];
 
-$input     = $_POST['input'];
-$length    = min(2000, $_POST['length']);
-$coherence = min(10, $_POST['coherence']);
+$input     = substr($_REQUEST['input'], 0, 100000);
+$length    = min($_REQUEST['length'], 2000);
+$coherence = min($_REQUEST['coherence'], 20);
 $output    = '';
 
 if ($input) {
   $relations = w_build_relations($input, $coherence);
-#  $profile = tabulate_profile($relations);
   $output = w_generate_text($relations, $length);
 }
 
+$input = htmlentities($input);
+$output = htmlentities($output);
+
+if ($_REQUEST['json']) {
+  mime('json');
+  print json_encode(['output' => $output]);
+  exit;
+}
+
 $page['title'] = 'Markov text generator';
-$page['navigation'] = theme_navigation(navigation_tree());
 $page['meta']['description'] = 'This page runs an input text through a random word scrambler.';
 $page['meta']['keywords'] = array('markov chain', 'neural net', 'artificial intelligence', 'convert');
 
+$page['scripts'] = ['style/jquery', 'style/jquery.form', 'style/ajax'];
+
 $page['content'] = <<<DOC
+    <p>This converter will read your input text and build a probability function. This function indicates how likely a certain word follows another given word. It will then randomly generate a text by using this probability function.</p>
+    <form action='' method='post'>
+      <input type='hidden' name='json' value='' />
+      <p><textarea name="input" rows="5" style="width:100%">$input</textarea></p>
+      <p>Generate <input type="text" name="length" value="$length" /> words.</p>
+      <p>Chain length: <input type="text" name="coherence" value="$coherence" /> words. (Lower = less coherent, higher = less deviation from the input text. Anything above 10 is likely to result in a word-for-word excerpt, depending on input size.)</p>
+      <p><input type="submit" value="Generate" /></p>
+    </form>
 
-  <p class="maintext">This converter will read your input text and build a probability function. This function indicates how likely a certain word follows another given word. It will then randomly generate a text by using this probability function.</p>
-
-  <form action='' method='post'>
-
-    <p class="maintext">
-      <textarea name="input" rows="10" cols="60">$input</textarea>
-    </p>
-    
-    <p>Give me <input type="text" name="length" value="$length" /> words.</p>
-    <p>Chain length: <input type="text" name="coherence" value="$coherence" /> words. (Lower = less coherent, higher = less deviation from the input text. Anything above 10 is likely to result in a word-for-word excerpt, depending on input size.)</p>
-    <p class="maintext"><input type="submit" value="Yank that Markov chain" /></p>
-  </form>
-
-  <h3>Output</h3>
-  <p class="maintext"><textarea rows="10" cols="60">$output</textarea></p>
+    <h3>Output</h3>
+    <div id="output" class="filled-box codeblock" style="white-space:pre-line">$output</div>
 DOC;
 
 print theme_page($page);
