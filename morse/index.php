80954cb086fd3204a7cbc7a6f5188ef8f5a65efc
Aran <aran@ermarian.net>
2014-12-02 03:23:55 +0000

    Replaced Morse AJAX with local JS.
    Seriously, why would you need to call a server for this.

--- morse/index.php
+++ morse/index.php
@@ -5,7 +5,7 @@ require_once 'net/mime.inc';
 require_once 'parsers/morse.inc';
 
 function is_morse($string) {
-	return preg_match('/^[\-\. \/]*$/', $string);
+  return preg_match('/^[\-\. \/]*$/', $string);
 }
 
 $input = $output = '';
@@ -18,14 +18,14 @@ $v = $_REQUEST + [
 
 if ($v['input']) {
   $input = trim($v['input']);
-	if (is_morse($input)) $output = morse_decode($input);
-	else $output = morse_encode($input);
+  if (is_morse($input)) $output = morse_decode($input);
+  else $output = morse_encode($input);
 }
 
 if ($v['json']) {
-	mime('json');
-	print json_encode(array('output' => $output));
-	exit;
+  mime('json');
+  print json_encode(array('output' => $output));
+  exit;
 }
 if ($v['auto']) {
   mime('json');
@@ -33,12 +33,14 @@ if ($v['auto']) {
   exit;
 }
 
+$THIS = dirname($_SERVER['SCRIPT_NAME']);
+
 $page['meta']['keywords'] = ['morse', 'code', 'morse code', 'converter', 'encoder', 'conversion', 'convert'];
 $page['meta']['extra'] = <<<DOC
 <link rel="search" type="application/opensearchdescription+xml" title="Morse Convert" href="services/converters/morse/morse" />
 <link rel="shortcut icon" type="image/bmp" href="data:image/bmp;base64,Qk32AAAAAAAAAHYAAAAoAAAAEAAAABAAAAABAAQAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAgAAAAICAAIAAAACAAIAAgIAAAICAgADAwMAAAAD%2FAAD%2FAAAA%2F%2F8A%2FwAAAP8A%2FwD%2F%2FwAA%2F%2F%2F%2FAAAAAAAAAAAAC7u7u7u7u7ALu7u7u7u7sAu7u7u7u7uwCwC7u7u7u7ALALu7u7u7sAu7u7u7u7uwC7u7u7u7u7ALu7sAAAALsAu7uwAAAAuwC7u7u7u7u7ALu7u7u7u7sAu7u7u7u7uwC7u7u7u7u7ALu7u7u7u7sAAAAAAAAAAA" />
 DOC;
-$page['scripts'] = ['js/jquery', 'js/jquery.form', 'js/ajax'];
+$page['scripts'] = ['js/jquery', "$THIS/morse.ui.js"];
 $page['content'] = <<<DOC
   <p>You may enter a morse code to convert here, or a normal text to convert to
   morse. Format: <code>"."</code> for short, <code>"-"</code> for long, any
@@ -48,7 +50,7 @@ $page['content'] = <<<DOC
     <input type="hidden" name="json" value="0" />
     <p style="text-align:center; vertical-align:middle">
       <input id="input" type="text" name="input" size="80" value="{$input}" />
-      <input type="submit" value="Convert" style="vertical-align:middle" />
+      <input id="submit" type="submit" value="Convert" style="vertical-align:middle" />
     </p>
   </form>
   <h3>Output</h3>
---
+++ morse/morse.ui.js
@@ -0,0 +1,89 @@
+encode = function(x) {
+  var output = [];
+  x = x.toUpperCase().trim();
+  for (i in x) {
+    if (alphabet[x[i]]) output.push(alphabet[x[i]]);
+    else return 'Error: Bad character "' + x[i] + '" at #' + i + '.';
+  }
+  return output.join(' ');
+}
+
+decode = function(x) {
+  var output = [];
+  x = x.trim().split(/\s+/);
+  for (i in x) {
+    if (alphabet_rev[x[i]]) output.push(alphabet_rev[x[i]]);
+    else return 'Error: Bad code "' + x[i] + '" at #' + i + '.';
+  }
+  return output.join('');
+}
+
+code = function(x) {
+  x = x.toUpperCase().trim();
+  if (!x) return "";
+  return x.match(/[A-Za-z0-9]/) ? encode(x) : decode(x);
+}
+
+alphabet = {
+  'A': '.-',
+  'B': '-...',
+  'C': '-.-.',
+  'D': '-..',
+  'E': '.',
+  'F': '..-.',
+  'G': '--.',
+  'H': '....',
+  'I': '..',
+  'J': '.---',
+  'K': '-.-',
+  'L': '.-..',
+  'M': '--',
+  'N': '-.',
+  'O': '---',
+  'P': '.--.',
+  'Q': '--.-',
+  'R': '.-.',
+  'S': '...',
+  'T': '-',
+  'U': '..-',
+  'V': '...-',
+  'W': '.--',
+  'X': '-..-',
+  'Y': '-.--',
+  'Z': '--..',
+  '0': '-----',
+  '1': '.----',
+  '2': '..---',
+  '3': '...--',
+  '4': '....-',
+  '5': '.....',
+  '6': '-....',
+  '7': '--...',
+  '8': '---..',
+  '9': '----.',
+  '.': '.-.-.-',
+  ',': '--..--',
+  '?': '..--..',
+  ' ': '/',
+  '-': '-....-',
+  ')': '-.--.-'
+};
+
+alphabet_rev = {};
+for (x in alphabet) alphabet_rev[alphabet[x]] = x;
+
+(function($) {
+  $(document).ready(function() {
+    var input = $('#input');
+    var output = $('#output');
+    var update = function() {
+      var text = input.val();
+      output.text(code(text));
+    }
+    input.on('keypress change keyup', function() {
+      update();
+    });
+    update();
+    $('#submit').hide();
+  });
+})($);
